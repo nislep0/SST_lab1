41ee4c358de22ceafe7b9f9d510a0990376f60b7
nislep0 <ruden1603@gmail.com>
2025-11-22 20:07:32 +0200

    add more tests

--- tests/test_game_ok.py
+++ tests/test_game_ok.py
@@ -33,6 +33,52 @@ def test_evolve_no_change():
     result = evolve(initial_field, 0)
     assert result == initial_field 
 
+def test_evolve_still_life():
+    initial_field = [
+        "......",
+        ".xx...",
+        ".xx...",
+        "......",
+    ]
+    exp = [
+        "......",
+        ".xx...",
+        ".xx...",
+        "......",
+    ]
+    result = evolve(initial_field, 5)
+    assert result == exp
 
+def test_evolve_oscillator():    
+    initial_field = [
+        ".....",
+        "..x..",
+        "..x..",
+        "..x..",
+        ".....",
+    ]
+    exp = [
+        ".....",
+        ".....",
+        ".xxx.",
+        ".....",
+        ".....",
+    ]
+    result = evolve(initial_field, 1)
+    assert result == exp
+
+def test_full_evolution():
+    initial_field = [
+            "xxx",
+            "...",
+            "...",
+        ]
+    exp = [
+            "xxx",
+            "xxx",
+            "xxx",
+        ]
+    result = evolve(initial_field, 1)
+    assert result == exp
 
 
